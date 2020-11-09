c7f0f258b3bc2cac99b59b4ea008f8b548765964
Thiago Lagden <lagden@gmail.com>
2020-11-09 03:13:43 -0300

    fix parse

--- test/index.js
+++ test/index.js
@@ -3,9 +3,10 @@
 const test = require('ava')
 const stockQuote = require('..')
 
-test('vale3', async t => {
-	const r = await stockQuote('vale3')
-	t.is(r.papel, 'BOV:VALE3')
+test('quote', async t => {
+	const r = await stockQuote('mglu3')
+	console.log(r)
+	t.is(r.papel, 'BOV:MGLU3')
 })
 
 test('throws', async t => {
